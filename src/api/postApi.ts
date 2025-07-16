c757d51960f98cde7886d88c5a282b1d7947a3e3
Renaldis <renaldis142@gmail.com>
2025-07-16 21:00:02 +0700

    feat: add dropdownmenu

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -3,23 +3,23 @@ import axios from "axios";
 const BASE_URL = "https://suitmedia-backend.suitdev.com/api/ideas";
 
 export const getPosts = async ({
-  number,
-  size,
-  typeImage,
-  published,
+  number = 1,
+  size = 10,
+  typeImage = ["small_image", "medium_image"],
+  published = "-published_at",
 }: {
   number?: number;
   size?: number;
-  typeImage?: "small_image" | "medium_image";
-  published?: "published_at" | "-published_at";
+  typeImage?: string[];
+  published?: string;
 }) => {
-  const res = await axios.get(
-    `${BASE_URL}?page[number]=${number}&page[size]=${size}&append[]=${typeImage}&sort=${published}`
-  );
+  const params = new URLSearchParams();
+
+  params.append("page[number]", number.toString());
+  params.append("page[size]", size.toString());
+  typeImage.forEach((type) => params.append("append[]", type));
+  params.append("sort", published);
+
+  const res = await axios.get(`${BASE_URL}?${params.toString()}`);
   return res.data;
 };
-
-// export const getPostById = async (id) => {
-//   const res = await axios.get(`${BASE_URL}/posts/${id}`);
-//   return res.data;
-// };
